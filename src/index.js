97089ba94ecf9abd845814802de67af5aa93ef83
Michael <431932+soulemike@users.noreply.github.com>
2025-10-04 00:15:12 +0000

    Facts

--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
 import * as core from "@actions/core";
-import * as github from "@actions/github";
+//import * as github from "@actions/github";
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput("who-to-greet");
-  core.info(`Hello ${nameToGreet}!`);
-
-  // Get the current time and set it as an output variable
-  const time = new Date().toTimeString();
-  core.setOutput("time", time);
+  async function getCatFact() {
+    const response = await fetch('https://catfact.ninja/fact');
+    const data = await response.json();
+    return data.fact;
+  }
+  core.setOutput("fact", `Cat fact: ${await getCatFact()}`);
 
   // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2);
-  core.info(`The event payload: ${payload}`);
+  //const payload = JSON.stringify(github.context.payload, undefined, 2);
+  //core.info(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
 }
